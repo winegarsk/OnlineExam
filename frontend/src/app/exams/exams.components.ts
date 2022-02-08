f78b6fde2a2ce80b8092557f4a866dccb83f09d8
winegarsk <33106750+winegarsk@users.noreply.github.com>
2022-02-08 13:37:17 -0800

    containers running, need to build frontend up

--- frontend/src/app/exams/exams.components.ts
+++ frontend/src/app/exams/exams.components.ts
@@ -5,32 +5,12 @@ import {Exam} from './exam.model';
 import {ExamsApiService} from './exams-api.service';
 import { AuthService } from '@auth0/auth0-angular';
 import { DOCUMENT } from '@angular/common';
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'exams',
-  template: `
-    <div>
-      <ul *ngIf="auth.user$ | async as user">
-        <li>Hello {{ user.name }}</li>
-      </ul>
-      <button routerLink="/new-exam">New Exam</button>
-      <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
-      <button (click)="auth.logout({ returnTo: document.location.origin })">
-        Log out
-      </button>
-    </ng-container>
-
-    <ng-template #loggedOut>
-      <button (click)="auth.loginWithRedirect()">Log in</button>
-    </ng-template>
-      
-      <ul>
-        <li *ngFor="let exam of examsList">
-          {{exam.title}}
-        </li>
-      </ul>
-    </div>
-  `
+  templateUrl: './exams.component.html',
+  styleUrls: ['../app.component.css']
 })
 export class ExamsComponent implements OnInit, OnDestroy {
   examsListSubs!: Subscription|undefined;
